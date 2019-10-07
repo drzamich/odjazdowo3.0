fb0cabfbc374ab5b872768a28c32a14ef3e2cc21
drzamich <drzamich@gmail.com>
2019-10-07 13:06:01 +0200

    add server with 'Hello World'

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app: express.Application = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
