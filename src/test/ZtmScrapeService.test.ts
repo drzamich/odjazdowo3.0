d894826ca2b4ea9546b2735c854953b385762450
drzamich <drzamich@gmail.com>
2019-11-16 17:13:53 +0100

    set up tests

---
+++ src/test/ZtmScrapeService.test.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+
+import { Mock } from 'moq.ts';
+import { ZtmScrapeService } from '../service';
+
+describe(ZtmScrapeService.name, () => {
+  it('smoke test', () => {
+    expect(true).toBeTruthy();
+  });
+});
